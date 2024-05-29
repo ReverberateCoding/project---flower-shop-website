e524344bdda4301daed5fb4bff27932ce1c3f4ce
reverberate <raphaellimfuencoding@gmail.com>
2024-05-29 19:49:44 +0800

    commit3

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__, template_folder="templates")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
